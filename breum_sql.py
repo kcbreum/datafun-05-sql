4b740a554877b517a55a5cf8c9337146835f9eed
kcbreum <kcwhite1993@gmail.com>
2024-02-04 12:09:57 -0600

    final updates

--- breum_sql.py
+++ breum_sql.py
@@ -26,3 +26,36 @@ logging.basicConfig(filename='log.txt', level=logging.DEBUG, filemode='a', forma
 
 logging.info("Program started")
 logging.info("Program ended")
+
+# Define functions
+
+def execute_sql_from_file(db_filepath, sql_file):
+    with sqlite3.connect(db_filepath) as conn:
+        with open(sql_file, 'r') as file:
+            sql_script = file.read()
+        conn.executescript(sql_script)
+        print(f"Executed SQL from {sql_file}")
+
+
+
+# Define Main function
+def main():
+    db_filepath = 'project.db'
+
+    # Create database schema and populate with data
+    execute_sql_from_file(db_filepath, 'create_tables.sql')
+    execute_sql_from_file(db_filepath, 'insert_records.sql')
+    execute_sql_from_file(db_filepath, 'update_records.sql')
+    execute_sql_from_file(db_filepath, 'delete_records.sql')
+    execute_sql_from_file(db_filepath, 'query_aggregation.sql')
+    execute_sql_from_file(db_filepath, 'query_filter.sql')
+    execute_sql_from_file(db_filepath, 'query_sorting.sql')
+    execute_sql_from_file(db_filepath, 'query_group_by.sql')
+    execute_sql_from_file(db_filepath, 'query_join.sql')
+
+    logging.info("All SQL operations completed successfully")
+
+
+# Conditional script
+if __name__ == '__main__':
+    main()
--- sql/query_join.sql
+++ sql/query_join.sql
@@ -1,4 +1,4 @@
 -- Example of an Inner join between authors and books tables
-SELECT authors.first_name, authors.last_name, books.title, books.year_published
+SELECT authors.first, authors.last, books.title, books.year_published
 FROM authors
 INNER JOIN books ON authors.author_id = books.author_id;
